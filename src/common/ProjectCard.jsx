ae755e689c5a2edf92865aded3c595a85644d906
abin0001 <abinj2003@gamail.com>
2024-07-12 01:50:30 +0530

    Updated Portfolio

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function ProjectCard({ src, link, h3, p }) {
+function ProjectCard({ src, link1, h3, p }) {
   return (
-    <a href={link} target="_blank">
+    <section>
       <img className="hover" src={src} alt={`${h3} logo`} />
       <h3>{h3}</h3>
       <p>{p}</p>
-    </a>
+      <div className='Buttons'>
+        <a href={link1}><button>Github</button></a>
+        <a href={link1}><button>Live</button></a>
+      </div>
+    </section>
   );
+
+  
 }
 
 export default ProjectCard;
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -3,44 +3,21 @@ import styles from './ContactStyles.module.css';
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
-      <h1 className="sectionTitle">Contact</h1>
-      <form action="">
-        <div className="formGroup">
-          <label htmlFor="name" hidden>
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            required
-          />
+      <h1 className="sectionTitle">Get in Touch</h1>
+
+      <div className={styles.cards}>
+        <div className={styles.card}>
+
+          <p><a href="mailto:examplemail@gmail.com">Gmail</a></p>
+
         </div>
-        <div className="formGroup">
-          <label htmlFor="email" hidden>
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            required
-          />
+        <div className={styles.card}>
+
+          <p><a href="https://www.linkedin.com/in/simi-gracia-sunil-christopher-47362720a/">LinkedIn</a></p>
+
         </div>
-        <div className="formGroup">
-          <label htmlFor="message" hidden>
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            required></textarea>
-        </div>
-        <input className="hover btn" type="submit" value="Submit" />
-      </form>
+      </div>
+
     </section>
   );
 }
